2e28103bef2f33e01672e58994b5b33a4063558e
영진 최 <chyj0318@naver.com>
2018-11-23 17:45:25 +0900

    ㅎㅎ

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Panel panel1 = new Panel(); // 도서정보
+        Panel panel2 = new Panel(); // 회원정보
+        Panel panel3 = new Panel(); // 도서관리
+        Panel panel4 = new Panel(); // 연체관리
+        Panel panel1u = new Panel(); // 도서정보
+        Panel panel2u = new Panel(); // 대여목록
+        Panel panel3u = new Panel(); // 나의정보
+        Panel panel4u = new Panel(); // 도서위치MAP
+
+        public Form1()
+        {
+            InitializeComponent();
+            Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+
+            
+        }
+
+
+    }
+}
